fb4352c05bd478d5749a82930961340f4a5986b6
sergere15 <43388544+sergere15@users.noreply.github.com>
2019-12-28 01:59:47 +0300

    Upload Project

---
+++ mysite/polls/views.py
@@ -0,0 +1,71 @@
+#from django.http import HttpResponse
+#from django.template import loader
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+import os
+import boto3
+import botocore.session
+import requests 
+import json
+
+from .models import Choice, Question
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):   
+    model = Question
+    template_name = 'polls/detail.html'
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    user = get_object_or_404(User, username = request.user.username)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        URL = " https://ohgtkcq8mk.execute-api.us-east-1.amazonaws.com/Start"
+        PARAMS = {'question':question.question_text, 'name': user.first_name} 
+        PARAMS =json.dumps(PARAMS)
+        url = URL
+        r = requests.post(url, PARAMS) 
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
